020a39361c0575741d63094cc857fa952e297fdc
mayday77-77 <mayday77@gmail.com>
2016-03-27 21:12:48 +0800

    Starting to refactor format_date

--- lib/listable.rb
+++ lib/listable.rb
@@ -3,5 +3,21 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+
+  # refactor format_date
+  def format_date(first_date, options = {})
+  	puts options.class
+  	first_date ? first_date.strftime("%D") : "No due date"
+#  	dates = @start_date.strftime("%D") if @start_date
+ #   dates << " -- " + @end_date.strftime("%D") if @end_date
+  #  dates = "N/A" if !dates
+   # return dates
   
+  end
+
+  #refactor format_priority
+  def format_priority
+
+  end
+
 end
